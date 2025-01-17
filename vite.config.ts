7c5958bd869284ca2d71f5be944d78e0839c50e3
ranjithkuamr225 <59768042+ranjithkuamr225@users.noreply.github.com>
2025-01-17 17:14:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<repository-name>/', // Replace <repository-name> with your GitHub repository name
+  base: '/studio/', // Replace <repository-name> with your GitHub repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
