1f69948def5d2701f5999a91e5f0fb6b9c02a50e
ranjithkuamr225 <59768042+ranjithkuamr225@users.noreply.github.com>
2025-01-17 18:04:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/studio/', // Replace with your GitHub repository name
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'index.html', dest: '' }, // Copy index.html to the output directory for 404.html
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'dist', // Ensure the build output is set correctly
+    emptyOutDir: true, // Clears old builds before generating a new one
   },
-}); 
+});
